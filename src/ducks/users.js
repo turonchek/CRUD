265cf905259e958c4f690233c0dc54e8e94f846b
Elena <turonchek09@gmail.com>
2023-01-08 20:19:30 +0200

    user adding feature was created

--- src/ducks/users.js
+++ src/ducks/users.js
@@ -16,28 +16,23 @@ const usersSlice = createSlice({
             state.loading = false
         },
         deleteUser: (state, action) => {
+            console.log(action)
             const deleteUser = state.users.filter((user) => user.id !== action.payload)
             state.users = deleteUser;
+            state.loading = false
+        },
+        addUser: (state,action) => {
+            state.users.push(action.payload);
             state.loading = false
         }
     }
 })
-
-// export const getAllUsersAsync = () => async (dispatch) => {
-//     try {
-//         const response = await axios.get(`${process.env.REACT_APP_API}`);
-//         dispatch(getAllUsers(response.data));
-//     } catch (err) {
-//         throw new Error(err);
-//     }
-// }
 
 export const getAllUsersAsync = () => {
     return function (dispatch) {
         axios
             .get(`${process.env.REACT_APP_API}`)
             .then((resp) => {
-                console.log(resp);
                 dispatch(getAllUsers(resp.data));
             })
             .catch((error) => console.log(error))
@@ -49,9 +44,18 @@ export const deleteUserAsync = (id) => {
         axios
             .delete(`${process.env.REACT_APP_API}/${id}`)
             .then((resp) => {
-                console.log(resp);
-                dispatch(deleteUser());
-                dispatch(getAllUsers());
+                dispatch(deleteUser(id));
+            })
+            .catch((error) => console.log(error))
+    }
+}
+
+export const addUserAsync = (user) => {
+    return function (dispatch) {
+        axios
+            .post(`${process.env.REACT_APP_API}`, user)
+            .then((resp) => {
+                dispatch(addUser(user));
             })
             .catch((error) => console.log(error))
     }
@@ -61,6 +65,6 @@ const { reducer, actions } = usersSlice;
 
 export default reducer;
 
-export const { getAllUsers, deleteUser } = actions;
+export const { getAllUsers, deleteUser, addUser } = actions;
 
 export const selectUsers = (rootState) => rootState.users;
--- src/pages/AddUserPage.js
+++ src/pages/AddUserPage.js
@@ -3,17 +3,42 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import { Button } from '@mui/material';
 import { useNavigate } from 'react-router';
+import { useDispatch } from 'react-redux';
+import { addUserAsync } from '../ducks/users';
 
 export const AddUserPage = () => {
 
     const navigate = useNavigate();
+    const dispatch = useDispatch();
 
-    const [state, setstate] = useState({
+    const [state, setState] = useState({
         name:"",
         email:"",
         contact:"",
         address:"",
     });
+
+    const [error, setError] = useState(false);
+    console.log(error)
+
+    const handleInput = (e) => {
+        const {value, name} = e.target;
+        setState({
+            ...state,
+            [name]:value
+        })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if(!name || !address || !email || !contact) {
+            setError(true)
+        }else {
+            dispatch(addUserAsync(state))
+            navigate("/")
+            setError(false)
+        }
+    }
 
     const { name, email, contact, address} = state;
 
@@ -35,34 +60,47 @@ export const AddUserPage = () => {
                 }}
                 noValidate
                 autoComplete="off"
+                onSubmit={handleSubmit}
             >
                 <TextField 
                     id="standard-basic" 
                     type="text" 
                     label="Name" 
                     variant="standard" 
-                    value={name} />
+                    value={name}
+                    name="name" 
+                    onChange={handleInput}
+                    />
                 <br />
                 <TextField 
                     id="standard-basic" 
                     type="email" 
                     label="Email" 
                     variant="standard" 
-                    value={email} />
+                    value={email}
+                    name="email" 
+                    onChange={handleInput}
+                    />
                 <br />
                 <TextField 
                     id="standard-basic" 
                     type="number" 
                     label="Contact" 
                     variant="standard" 
-                    value={contact} />
+                    value={contact}
+                    name="contact"
+                    onChange={handleInput}
+                    />
                 <br />
                 <TextField 
                     id="standard-basic" 
                     type="text" 
                     label="Address" 
                     variant="standard" 
-                    value={address} />
+                    value={address}
+                    name="address" 
+                    onChange={handleInput}
+                    />
                 <br />
                 <Button 
                     variant='contained' 
